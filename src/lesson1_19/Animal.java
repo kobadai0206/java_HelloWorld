52a2c6b3794a36ba3e598f084458fcd2528ea3e8
kobadai0206 <kobadai0206@gmail.com>
2023-04-21 09:41:00 +0900

    commit

--- src/lesson1_19/Animal.java
+++ src/lesson1_19/Animal.java
@@ -1,4 +1,6 @@
 package lesson1_19;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Animal {
 
@@ -7,11 +9,15 @@ public class Animal {
 		Dog dog = new Dog();
 		System.out.println(dog.animal);
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
-		Dog num = new Dog();
-		System.out.println(dog.num);
+		Dog d = new Dog(4);
+		System.out.println(d.num);
 
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
-
+		LocalDateTime date = LocalDateTime.now();
+		DateTimeFormatter datetime = DateTimeFormatter.ofPattern("yyyy/MM/dd/ H:m:s");
+		String datetimeformated = datetime.format(date);
+		System.out.println(datetimeformated);
+        
 	}
 
 }
